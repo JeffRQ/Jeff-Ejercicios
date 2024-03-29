f59df4538c90a231a09e4ec1bc3661b5bcdfb1e5
Jeff-Project <jj.ramirezq@uea.edu.ec>
2024-03-28 23:01:33 -0500

    deber S16

---
+++ Tarea S16/Leer_datos.py
@@ -0,0 +1,29 @@
+# Abrir el archivo en modo lectura
+archivo = open("My_notes.txt", "r")
+
+# Leer los datos del archivo y almacenarlos en una variable
+contenido_anterior = archivo.read()
+
+archivo.close()
+
+# Abrir el archivo en modo escritura
+archivo = open("My_notes.txt", "w")
+
+# Escribir el nuevo contenido en el archivo, junto con el contenido anterior
+archivo.write(contenido_anterior + "\n")
+archivo.write("Celular: 0994438439")
+
+# Cerrar el archivo después de la escritura
+archivo.close()
+
+# Abrir el archivo nuevamente en modo lectura
+archivo = open("My_notes.txt", "r")
+
+# Leer los datos del archivo actualizado y almacenarlos en una variable
+contenido_actualizado = archivo.read()
+
+# Imprimir actializacion
+print(contenido_actualizado)
+
+# Cerrar el archivo después de la última lectura
+archivo.close()
